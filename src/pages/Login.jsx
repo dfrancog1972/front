606c30a8d2ab07052548d65a642ee547a7122755
dfranco1972 <daniel.franco@fondocnog.com.mx>
2024-02-29 19:34:30 -0600

    antes de usar redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,59 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <>
+      <section className="heading">
+        <h4>
+          <FaUser /> Entrar a la App
+        </h4>
+        <p>Por favor registra tu usuario</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Por favor escriba su email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              placeholder="Por favor escribe tu password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Registrarme
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,45 +1,85 @@
-import { useState, useEffect } from 'react'
-import { FaUser } from 'react-icons/fa'
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Register = () => {
-  const [formData, setFormData] = useState({ 
-      name: '', 
-      email: '', 
-      password: '', 
-      password2:'' 
-  })
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-  const { name, email, password, password2 } = formData
-  
+  const { name, email, password, password2 } = formData;
+
   const onChange = (e) => {
-    setFormData((prevState) =>({
+    setFormData((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value
-    }))
-  }
-  
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
-      <section className='heading'>
-        <h4><FaUser /> Registrar Usuario</h4>
-        <p>Por favor crea un usuario</p>        
+      <section className="heading">
+        <h4>
+          <FaUser /> Registrar Usuario
+        </h4>
+        <p>Por favor crea un usuario</p>
       </section>
-      <section className='form'>
-        <form>
+      <section className="form">
+        <form onSubmit={onSubmit}>
           <div className="form-group">
-            <input 
-              type='text' 
-              className='form-control' 
-              id='name'
-              name='name'
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
               value={name}
-              placeholder='Por favor ecribe el Nombre Completo'
-              onChange={onChange}              
+              placeholder="Por favor ecribe el Nombre Completo"
+              onChange={onChange}
             />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Por favor escriba su email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              placeholder="Por favor escribe tu password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password2"
+              name="password2"
+              id="password2"
+              value={password2}
+              placeholder="Por favor confirma tu password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Registrarme
+            </button>
           </div>
         </form>
       </section>
     </>
-  )
-}
-export default Register
+  );
+};
+export default Register;
